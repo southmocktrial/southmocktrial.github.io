bd27608ed74de499fb46af45f4f2c2773ca6a654
Daniel Hu <d4niel.hu14@gmail.com>
2020-09-27 01:18:26 -0400

    start jumbo

--- js/app.js
+++ js/app.js
@@ -1,7 +1,9 @@
-$('.navTrigger').click(function () {
-    $(this).toggleClass('active');
-    console.log("Clicked menu");
-    $("#mainListDiv").toggleClass("show_list");
-    $("#mainListDiv").fadeIn();
-
+$(function () {
+    $(window).on('scroll', function () {
+        if ( $(window).scrollTop() > 10 ) {
+            $('.navbar').addClass('active');
+        } else {
+            $('.navbar').removeClass('active');
+        }
+    });
 });
